a4cdd0773c48d7f7e96528eeccaab057a6c4db5e
Rory Willox <rory.willox@gmail.com>
2023-07-19 23:01:31 +0100

    Add implementation for GetTotalPrice

--- Checkout/Checkout.cs
+++ Checkout/Checkout.cs
@@ -3,11 +3,14 @@
 /// <inheritdoc cref="ICheckout"/>
 public class Checkout : ICheckout
 {
+    private readonly SkuPriceService _skuPriceService;
+    
     /// <summary>
     /// Initializes a new instance of <see cref="Checkout"/>.
     /// </summary>
     public Checkout(SkuPriceService skuPriceService)
     {
+        _skuPriceService = skuPriceService;
         Items = new List<string>();
     }
     
@@ -15,7 +18,21 @@ public class Checkout : ICheckout
 
     public decimal GetTotalPrice()
     {
-        throw new NotImplementedException();
+        decimal totalPrice = 0;
+
+        foreach (string item in Items)
+        {
+            decimal? unitPrice =_skuPriceService.GetPrice(item);
+
+            if (unitPrice is null)
+            {
+                throw new InvalidOperationException($"Price not found for item: {item}");
+            }
+
+            totalPrice += (decimal)unitPrice;
+        }
+
+        return totalPrice;
     }
     
     public void Scan(string item) => Items.Add(item);
