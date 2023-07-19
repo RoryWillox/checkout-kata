d099228895a47aa524d39d6cb6781bdc75fe4d4b
Rory Willox <rory.willox@gmail.com>
2023-07-19 22:46:27 +0100

    Add SkuPriceService implementation

--- Checkout/SkuPrice.cs
+++ Checkout/SkuPrice.cs
@@ -1,9 +1,28 @@
 ﻿namespace Checkout;
 
+/// <summary>
+/// Represents the unit price of product.
+/// </summary>
 public class SkuPrice
 {
+    /// <summary>
+    /// Initializes a new instance of <see cref="SkuPrice"/>.
+    /// </summary>
+    /// <param name="sku">Stock keeping unit.</param>
+    /// <param name="price">Unit price.</param>
     public SkuPrice(string sku, decimal price)
     {
-        
+        Sku = sku;
+        Price = price;
     }
+    
+    /// <summary>
+    /// Stock keeping unit.
+    /// </summary>
+    public string Sku { get; private set; } 
+    
+    /// <summary>
+    /// Unit price.
+    /// </summary>
+    public decimal Price { get; private set; } 
 }
--- Checkout/SkuPriceService.cs
+++ Checkout/SkuPriceService.cs
@@ -1,14 +1,33 @@
 ﻿namespace Checkout;
 
+/// <summary>
+/// Represents a service to retrieve the unit price of a product.
+/// </summary>
 public class SkuPriceService
 {
+    private readonly Dictionary<string, decimal> _skuPrices;
+    
+    /// <summary>
+    /// Initializes a new instance of <see cref="SkuPriceService"/> using a list of <see cref="SkuPrice"/>.
+    /// </summary>
+    /// <param name="skuPrices">SKU unit prices.</param>
     public SkuPriceService(List<SkuPrice> skuPrices)
     {
-        
+        _skuPrices = skuPrices.ToDictionary(sp => sp.Sku, sp => sp.Price);
     }
 
+    /// <summary>
+    /// Get the unit price of an item.
+    /// </summary>
+    /// <param name="item">SKU of the item.</param>
+    /// <returns>Unit price of the product or null if no pricing information.</returns>
     public decimal? GetPrice(string item)
     {
-        throw new NotImplementedException();
+        if (!_skuPrices.ContainsKey(item))
+        {
+            return null;
+        }
+
+        return _skuPrices[item];
     }
 }
