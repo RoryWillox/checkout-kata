eb45732eb19fc6b55559b65dbd8a391e37d9dc57
Rory Willox <rory.willox@gmail.com>
2023-07-20 21:26:42 +0100

    Add ProductOfferService implementation

--- Checkout/ProductOffer.cs
+++ Checkout/ProductOffer.cs
@@ -1,9 +1,35 @@
 ﻿namespace Checkout;
 
+/// <summary>
+/// Represents a special offer for a product.
+/// </summary>
 public class ProductOffer
 {
+    /// <summary>
+    /// Initializes a new instance of <see cref="ProductOffer"/>.
+    /// </summary>
+    /// <param name="sku">Stock keeping unit.</param>
+    /// <param name="quantity">Quantity of products the offer applies to.</param>
+    /// <param name="specialPrice">Price of the products after offer is applied.</param>
     public ProductOffer(string sku, int quantity, decimal specialPrice)
     {
-        
+        Sku = sku;
+        Quantity = quantity;
+        SpecialPrice = specialPrice;
     }
+
+    /// <summary>
+    /// Stock keeping unit.
+    /// </summary>
+    public string Sku { get; private set; }
+
+    /// <summary>
+    /// Quantity of products the offer applies to.
+    /// </summary>
+    public int Quantity { get; private set; }
+
+    /// <summary>
+    /// Price of the products after offer is applied.
+    /// </summary>
+    public decimal SpecialPrice { get; private set; }
 }
--- Checkout/ProductOfferService.cs
+++ Checkout/ProductOfferService.cs
@@ -3,17 +3,19 @@
 /// <inheritdoc cref="IProductOfferService"/>
 public class ProductOfferService : IProductOfferService
 {
+    private readonly List<ProductOffer> _productOffers;
+    
     /// <summary>
     /// Initializes a new instance of <see cref="ProductOfferService"/> using a list of <see cref="ProductOffer"/>.
     /// </summary>
     /// <param name="productOffers">List of product offers.</param>
     public ProductOfferService(List<ProductOffer> productOffers)
     {
-        
+        _productOffers = productOffers;
     }
 
     public List<ProductOffer> GetOffers(string sku)
     {
-        throw new NotImplementedException();
+        return _productOffers.Where(po => po.Sku == sku).ToList();
     }
 }
